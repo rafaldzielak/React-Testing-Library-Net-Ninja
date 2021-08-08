32fe9db10a6a8f463775c2f96bdbb140cc79825e
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-08 12:56:47 +0200

    Assertions

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import TodoFooter from "../TodoFooter";
+
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => (
+  <BrowserRouter>
+    <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+  </BrowserRouter>
+);
+
+test("should render same text passed into title prop", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+  const paragraphElement = screen.getByText(/5 tasks left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+test("should render 'task' when number of incomplete tasks is one", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+test("should render 'task' when number of incomplete tasks is one", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeVisible();
+});
+
+test("should render 'task' when number of incomplete tasks is one", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toContainHTML("p");
+  expect(paragraphElement).toHaveTextContent("task");
+  expect(paragraphElement).not.toHaveTextContent("tasks");
+});
+
+test("should render 'task' when number of incomplete tasks is one", () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement.textContent).toBe("1 task left");
+});
