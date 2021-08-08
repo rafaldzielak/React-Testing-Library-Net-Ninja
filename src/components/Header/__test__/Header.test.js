e12cd3e000ee40c556d7faa3db148143822aa663
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-08-08 17:17:37 +0200

    Describe block

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -12,39 +12,40 @@ test("should render same text passed into title prop", () => {
 //   const headingElement = screen.getByRole("heading");
 //   expect(headingElement).toBeInTheDocument();
 // });
+describe("Header", () => {
+  test("should render same text passed into title prop", () => {
+    render(<Header title='My header' />);
+    const headingElement = screen.getByRole("heading", { name: "My header" });
+    expect(headingElement).toBeInTheDocument();
+  });
 
-test("should render same text passed into title prop", () => {
-  render(<Header title='My header' />);
-  const headingElement = screen.getByRole("heading", { name: "My header" });
-  expect(headingElement).toBeInTheDocument();
-});
+  test("should render same text passed into title prop", () => {
+    render(<Header title='My header' />);
+    const headingElement = screen.getByTitle("Header");
+    expect(headingElement).toBeInTheDocument();
+  });
 
-test("should render same text passed into title prop", () => {
-  render(<Header title='My header' />);
-  const headingElement = screen.getByTitle("Header");
-  expect(headingElement).toBeInTheDocument();
-});
+  test("should render same text passed into title prop", () => {
+    render(<Header title='My header' />);
+    const headingElement = screen.getByTestId("header-1");
+    expect(headingElement).toBeInTheDocument();
+  });
 
-test("should render same text passed into title prop", () => {
-  render(<Header title='My header' />);
-  const headingElement = screen.getByTestId("header-1");
-  expect(headingElement).toBeInTheDocument();
-});
+  test("should render same text passed into title prop", async () => {
+    render(<Header title='My header' />);
+    const headingElement = await screen.findByText("My header");
+    expect(headingElement).toBeInTheDocument();
+  });
 
-test("should render same text passed into title prop", async () => {
-  render(<Header title='My header' />);
-  const headingElement = await screen.findByText("My header");
-  expect(headingElement).toBeInTheDocument();
-});
+  test("should render same text passed into title prop", () => {
+    render(<Header title='My header' />);
+    const headingElement = screen.queryByText(/dogs/i);
+    expect(headingElement).not.toBeInTheDocument();
+  });
 
-test("should render same text passed into title prop", () => {
-  render(<Header title='My header' />);
-  const headingElement = screen.queryByText(/dogs/i);
-  expect(headingElement).not.toBeInTheDocument();
-});
-
-test("should render same text passed into title prop", () => {
-  render(<Header title='My header' />);
-  const headingElements = screen.getAllByRole("heading");
-  expect(headingElements).toHaveLength(2);
+  test("should render same text passed into title prop", () => {
+    render(<Header title='My header' />);
+    const headingElements = screen.getAllByRole("heading");
+    expect(headingElements).toHaveLength(2);
+  });
 });
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -8,34 +8,36 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => (
   </BrowserRouter>
 );
 
-test("should render same text passed into title prop", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+describe("Footer", () => {
+  test("should render same text passed into title prop", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-test("should render 'task' when number of incomplete tasks is one", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+  test("should render 'task' when number of incomplete tasks is one", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-test("should render 'task' when number of incomplete tasks is one", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeVisible();
-});
+  test("should render 'task' when number of incomplete tasks is one", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeVisible();
+  });
 
-test("should render 'task' when number of incomplete tasks is one", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toContainHTML("p");
-  expect(paragraphElement).toHaveTextContent("task");
-  expect(paragraphElement).not.toHaveTextContent("tasks");
-});
+  test("should render 'task' when number of incomplete tasks is one", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toContainHTML("p");
+    expect(paragraphElement).toHaveTextContent("task");
+    expect(paragraphElement).not.toHaveTextContent("tasks");
+  });
 
-test("should render 'task' when number of incomplete tasks is one", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement.textContent).toBe("1 task left");
+  test("should render 'task' when number of incomplete tasks is one", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement.textContent).toBe("1 task left");
+  });
 });
